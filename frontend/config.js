c314a26c465f17716d9cca0a6a6c157d0c5ef879
Thejammm <simon@archerhs.co.uk>
2026-04-24 09:35:10 +0100

    Update config.js
    Point frontend at Render backend

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // Edit this one value to point the frontend at your backend.
 // Dev:  http://localhost:3000
 // Prod: https://your-service.onrender.com   (or whatever Render gives you)
-window.API_BASE = 'http://localhost:3000';
+window.API_BASE = 'https://pdscope-api.onrender.com';
